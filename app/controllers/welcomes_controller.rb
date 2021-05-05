26010cc1bd1a9c3d6a768a11458e46e0d2ee5845
Dan Tuls <spimonid@gmail.com>
2021-05-05 10:49:59 -0500

    Installed Faraday

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,5 +1,9 @@
 class WelcomesController < ApplicationController
   def hello_method
-    render html: { message: "Welcome to Yelp scrape!" }
+    render json: { message: "Welcome to Yelp scrape!" }
+  end
+
+  def search_yelp
+    @search = Faraday.get("https://api.yelp.com/v3/businesses/search'")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/welcome_path", controller: "welcomes", action: "hello_method"
+  get "/yelp_search", controller: "searches", action: "search_yelp"
 end
